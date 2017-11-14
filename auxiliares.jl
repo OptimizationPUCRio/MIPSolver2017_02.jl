cf8ca27713bedef594e7f006b1542fd82cee1fb8
biancalace <bianca-lace@hotmail.com>
2017-11-14 03:06:33 -0200

    falta finalizar o while

--- auxiliares.jl
+++ auxiliares.jl
@@ -8,7 +8,7 @@ mutable struct node
   xLb::Vector{Int}
   zUb::Float64
   zLb::Int
-  problema::JuMP.Model
+  modelo::JuMP.Model
   status::Symbol
 end
 
@@ -17,11 +17,13 @@ mutable struct list
   Zsup
   Zinf
   xOt
+  isup
 end
 
 
 
 ### meu prob é de max
+#testei
 function mudaparamax(m::Model)
   #muda o c da função objetivo
   if m.objSense == :Min
@@ -30,7 +32,7 @@ function mudaparamax(m::Model)
   return m
 end
 
-#ainda vou mudar essa poda
+#testei
 function podainv(no::node)
   ###retorna 1 caso positivo (relaxação inviavel, realize a poda) e 0 caso a relaxação seja viavel e prossiga para outros testes
   if no.status == :Optimal
@@ -38,6 +40,7 @@ function podainv(no::node)
   end
   return 1
 end
+#testei
 function podaotim(no::node,lista::list)
   if testabin(no.xUb) == 1
     no.xLb=no.xUb
@@ -49,16 +52,18 @@ function podaotim(no::node,lista::list)
   end
   return 0
 end
+#testei (algo estranho acontece quando defino zinf, preciso fazer a listaini de novo)
 function podalimit(no::node,lista::list)
   if no.zUb < lista.Zinf
     return 1
   end
   return 0
 end
-function atualiza(lista::list)
+#testei
+function atualizaLupper(lista::list)
   tam=length(lista.L)
   maior=-1e10
-  #ind=0
+  ind=0
   for i in 1:tam
     if lista.L[i].zUb > maior
       maior=lista.L[i].zUb
@@ -66,8 +71,9 @@ function atualiza(lista::list)
     end
   end
   lista.Zsup=maior
-  #lista.isup=ind
+  lista.isup=ind
 end
+#testei
 function testabin(v::Vector)
     bin=1
     tam=length(v)
@@ -79,22 +85,26 @@ function testabin(v::Vector)
     return bin
 end
 
-function branch (no::node,lista::list)
-
-  if podainv(no) == 1 ||
+#duvida se uso o "ou", só uso se a função parar assim que encontra uma resposta positiva
+#testei
+function bound(no::node,lista::list)
+  if podainv(no) == 1 || podaotim(no,lista) == 1 || podalimit(no,lista) == 1
     return 1
   end
-  if podaotim(no,lista)
+  return 0
+end
 
+#testei
 function preencheno(mod::JuMP.Model)
   m=deepcopy(mod)
   status=solve(m, relaxation=true)
   xrel=copy(m.colVal)
   zub=copy(m.objVal)
-  no1=node(xrel,[],zub,-1e10,prob,status)
+  no1=node(xrel,[],zub,-1e10,m,status)
   return no1
 end
 
+#testei
 function achamaisfrac(no::node)
   tam=length(no.xUb)
   f=copy(no.xUb)
@@ -107,6 +117,27 @@ function achamaisfrac(no::node)
   return findmax(f)
 end
 
+#testei
+function inserefilhos(no::node,S::list)
+
+  max,ind=achamaisfrac(no)
+
+  mod1=deepcopy(no.modelo)
+  mod1.colLower[ind]=1
+  mod1.colUpper[ind]=1
+  filho1=preencheno(mod1)
+
+  mod2=deepcopy(no.modelo)
+  mod2.colLower[ind]=0
+  mod2.colUpper[ind]=0
+  filho2=preencheno(mod2)
+
+  push!(S.L,filho1,filho2)
+
+  atualizaLupper(S)
+
+  return S
+end
 
 function MILPSolver(m)
 
@@ -128,14 +159,76 @@ function MILPSolver(m)
     return no1.status,no1.xRel
   end
 
-  #como nao é bin nem inviavel podamos, escolho o x mais fracionario
-  xfrac,posfrac=achamaisfrac(no1)
+  #como nao é bin nem inviavel podamos (branch), escolho o x mais fracionario para fixar
+  #usamos uma lista vazia para inicialização
+  listaini=list([],1e10,-1e10,[],0)
 
-  #criamos então a lista com os dois nós originados a partir do x mais fracionario (em um no o fixamos em 0 e no outro o fizamos em 1)
+  #criamos a lista com dois nós
+  # esses nós foram originados a partir da fixação do x mais fracionario (em um no fixamos em 0 e no outro o fixamos em 1)
 
-  S=list([no1],no1.zUb,no1.zLb,[])
+  S=inserefilhos(no1,listaini)
 
-  if branch(list)==1
+  #agora começa o momento de branch e de bound, perceba que ja fiz o branch então dentro do while começamos com o bound
+  #paro quando encontrei uma solução viavel suficientemente proxima da solução da relaxação
+
+  ϵ = abs(S.Zsup - S.Zinf)
+  cont=0
+
+  while ϵ > 1e-5 && cont < 1e3
+
+    cont=cont+1
+
+    #seleciono um no para testar o bound
+    #a cada 5 vezes escolhendo o no do mesmo ramo (sempre retirando do final da lista), mudo para a esolha do que tem maior upperbound
+    #faço isso com o objetivo de não ficar preso em um nó que pode não ser o otimo escolho o que tem maior upper bound pq neste não corro
+    #o risco de analisar um nó que possui um upperbound inferior ao da função objetivo
+    h=(cont/5)
+
+    if floor(h) == h
+      no=S.L[S.isup]
+      deleteat!(S.L,S.isup)
+    else
+      no=pop!(S.L)
+    end
+
+    devobound=bound(no,S)
+    #a função bound atualiza o lowerbound no caso de poda por otimalidade
+
+    if devobound == 1
+      #atualizo o upperbound com os novos elementos e volto pro loop para escolher outro nó para analisar
+      atualizaLupper(S)
+
+    else
+      #como não consegui podar o ramo que contém esse nó devo fazer o branch do no o separando em 2 e fixando na variavel mais fracionaria
+      S=inserefilhos(no,S)
+      #a inserefilhos ja atualiza os bounds
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
